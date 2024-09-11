2fa7ba9417119c011d209f45933ac93ba665073a
Harsha Puriya <harshapuriya@gmail.com>
2024-09-11 09:31:11 +0530

    Episode-01

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
-const heading = React.createElement('h1', {}, "Hello World from React!");
+//using react to create element
+//{} - inside it we give attributes to our tag
+//heading is react element. The below line create object
+const headingOne = React.createElement('h1', {id: 'parent'}, [
+    React.createElement('h2', {id: 'child'}, "I am child"), 
+    React.createElement('h2', {id: 'child2'}, "I am child2")])
 
+const heading = React.createElement('h1', {}, "Hello World from React!");
+//using react dom to create rrot
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading);
+root.render(headingOne);

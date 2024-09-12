49ecaedd228fd368aa0dee2320363366410b5504
Harsha Puriya <harshapuriya@gmail.com>
2024-09-12 12:06:07 +0530

    Episode 3 Script, Component, babel

--- App.js
+++ App.js
@@ -11,4 +11,31 @@ const heading = React.createElement('h1', {}, "Hello World from React!");
 //using react dom to create rrot
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(headingOne);
+
+
+//JSX - HTML-like or XML-like syntax
+
+const jsxHeading = <h1 className="heading" tabIndex="5">Namaste React using JSX</h1>
+
+//React Functional Component
+
+const title = (
+    <h1 className="head">I am heading</h1>
+)
+const HeadingComponent = () => {
+    return <h1> React Functional Component</h1>
+}
+
+//component composition - component inside component
+const HeadingComponent2 = () => (
+    <div id="container">
+        {HeadingComponent()}
+        {title}
+        <HeadingComponent></HeadingComponent>
+        <HeadingComponent/>
+    <h1> React Functional Component</h1>
+    </div>
+)
+
+
+root.render(<HeadingComponent2/>);
